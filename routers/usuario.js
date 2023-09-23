9703ce64dc1dce7bccfe8eace0e323705fc5032a
Nirclaw <caicedonicolas150@gmail.com>
2023-09-23 18:30:53 -0500

    feat: :sparkles: Update and creation of users added

--- routers/usuario.js
+++ routers/usuario.js
@@ -1,4 +1,12 @@
 import { Router } from "express";
-
+import passport from "../jwt/passport.js";
+import { limite } from "../config/limit.js";
+import { VERSION } from "../config/variables.js";
+import { actualizar } from "../version/usuario.js";
 
 export const appUsuario = Router()
+appUsuario.use(limite(), passport.authenticate("bearer", { session: false }));
+
+appUsuario.put("/actualizar",VERSION({
+    "1.0.0":actualizar
+}))

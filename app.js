80889a835eaf5906d9e15e6c189c90ec963224ab
Nirclaw <caicedonicolas150@gmail.com>
2023-09-23 16:52:19 -0500

    feat: :sparkles: the register route is added

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ import { appLibros } from "./routers/libros.js";
 import { appUsuario } from "./routers/usuario.js";
 import { appLogin } from "./login/login.js";
 import cors from "cors";
+import { appRegister } from "./login/register.js";
 const appExpress = express();
 appExpress.use(express.json());
 appExpress.use(cors());
 
-appExpress.use("/log", appLogin);
+appExpress.use("/login", appLogin);
 appExpress.use("/libro", appLibros);
-
+appExpress.use("/register",appRegister)
 appExpress.use("/usuario", appUsuario);
 
 appExpress.listen(MY_SERVER, () => {
